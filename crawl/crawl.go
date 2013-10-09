619184128e8e3ee8b4c79eeb12dd6977fdba030e
akirayu101 <akirayu101@gmail.com>
2013-10-09 09:12:24 +0000

    modify gettoken api

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -1,80 +1,68 @@
 package crawl
 
 import (
-    "net/http"
-    "regexp"
-    "strings"
-    "encoding/base64"
-    "log"
-    "io/ioutil"
-    "strconv"
-    "fmt"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strconv"
+	"strings"
 )
-//google api
-func Crawl_google(page string) string{
-    client := &http.Client{}
-    req, err := http.NewRequest("GET",page,nil)
-    if err != nil{
-        log.Fatalln(err)
-    }
-    req.Header.Set("User-Agent","Mozilla/5.0 (Macintosh;Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36")
-    resp, err := client.Do(req)
-    if err != nil {
-        log.Fatalln(err)
-    }
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        log.Fatalln(err)
-    }
 
-    return string(body)
+//google api
+func Crawl_google(page string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", page, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh;Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.95 Safari/537.36")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	return string(body)
 }
 
-func DecodeGoogleImage(page string){
-    v, _ := regexp.Compile(`jpeg;base64,[A-Za-z0-9+/\\]+`)
-    result := v.FindAllStringSubmatch(page,15)
-    for i, v := range result{
-        str := strings.Replace(v[0],`\x3d`,`=`,-1)
-        data, err := base64.StdEncoding.DecodeString(str[12:])
-        if err != nil {
-            log.Fatal("error:", err)
-        }
+func DecodeGoogleImage(page string) {
+	v, _ := regexp.Compile(`jpeg;base64,[A-Za-z0-9+/\\]+`)
+	result := v.FindAllStringSubmatch(page, 15)
+	for i, v := range result {
+		str := strings.Replace(v[0], `\x3d`, `=`, -1)
+		data, err := base64.StdEncoding.DecodeString(str[12:])
+		if err != nil {
+			log.Fatal("error:", err)
+		}
 
-        err = ioutil.WriteFile(strconv.Itoa(i)+".jpeg", data, 0644)
-    }
+		err = ioutil.WriteFile(strconv.Itoa(i)+".jpeg", data, 0644)
+	}
 }
 
 //facebook api
-type AccessToken struct {
-	Token string
-	Expiry int64
-}
-
 func readHttpBody(response *http.Response) string {
 	fmt.Println("Reading body")
 	body, _ := ioutil.ReadAll(response.Body)
-  fmt.Println(string(body))
 	return string(body)
 }
 
 //Converts a code to an Auth_Token
-func GetAccessToken() AccessToken {
+func GetAccessToken() string {
 	fmt.Println("GetAccessToken")
 	//https://graph.facebook.com/oauth/access_token?client_id=YOUR_APP_ID&redirect_uri=YOUR_REDIRECT_URI&client_secret=YOUR_APP_SECRET&code=CODE_GENERATED_BY_FACEBOOK
 	response, err := http.Get("https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&client_id=494717897285592&client_secret=30228f96eb5f6692d88bb41c184c6b59")
+	var token string
 	if err == nil {
-		auth := readHttpBody(response)
-	    var token AccessToken
-	    tokenArr := strings.Split(auth, "&")
-	    token.Token = strings.Split(tokenArr[0], "=")[1]
-	    expireInt,err := strconv.Atoi(strings.Split(tokenArr[1],"=")[1])
-	    if (err == nil) {
-		    token.Expiry = int64(expireInt)
-		}
-	    return token
+		content := readHttpBody(response)
+		token = strings.Split(content, "=")[1]
 	}
-	var token AccessToken
+
 	return token
 }
-
